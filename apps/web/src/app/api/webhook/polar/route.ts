e7e64735b2c8b7d847b121055f17f0fe06542358
Pontus Abrahamsson <pontus@lostisland.co>
2025-03-19 07:06:13 +0100

    Update route.ts

--- apps/web/src/app/api/webhook/polar/route.ts
+++ apps/web/src/app/api/webhook/polar/route.ts
@@ -6,30 +6,6 @@ export const POST = Webhooks({
   webhookSecret: process.env.POLAR_WEBHOOK_SECRET!,
   onPayload: async (payload) => {
     switch (payload.type) {
-      case "checkout.updated": {
-        if (!payload.data.metadata.organizationId) {
-          console.error("Organization ID is missing");
-          break;
-        }
-
-        const tier = getTierFromProductId(payload.data.productId);
-
-        if (!tier) {
-          console.error("Invalid product ID", payload.data.productId);
-          break;
-        }
-
-        await updateOrganization({
-          id: payload.data.metadata.organizationId as string,
-          polarCustomerId: payload.data.customerId!,
-          email: payload.data.customerEmail ?? undefined,
-          tier,
-          plan: "pro",
-        });
-
-        break;
-      }
-
       case "subscription.updated": {
         const tier = getTierFromProductId(payload.data.productId);
 
